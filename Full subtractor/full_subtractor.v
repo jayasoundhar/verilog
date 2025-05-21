242f85f38b4d155bc5565c8315ba47b745c29d34
jayasoundhar <148468173+jayasoundhar@users.noreply.github.com>
2025-05-21 15:15:13 +0530

    Delete Full subtractor directory

--- Full subtractor/full_subtractor.v
+++
@@ -1,6 +0,0 @@
-// Code your design here
-// Code your design here
-module full_subtractor(output d,b_out, input a,b,b_in);
-  assign d=a^b^b_in;
-  assign b_out= (b_in&b) | (~a&(b_in|b));
-endmodule
