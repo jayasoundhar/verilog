fa134d564161c2f527ed1f15640af38f085ade2f
jayasoundhar <148468173+jayasoundhar@users.noreply.github.com>
2025-05-21 15:09:39 +0530

    Create half_sub_tb.v

---
+++ SUBTRACTOR/HALF SUB/half_sub_tb.v
@@ -0,0 +1,35 @@
+//testbench for half subtractor
+module t_half_subtractor();
+  reg t_a;
+  reg t_b;
+  wire t_c;
+  wire t_s;
+  
+  half_subtractor dut(.a(t_a), .b(t_b), .c(t_c), .s(t_s));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+  end
+  
+  initial begin
+    //0
+    t_a=0;
+    t_b=0;
+    #5
+    //1
+    t_a=0;
+    t_b=1;
+    #5
+    //2
+    t_a=1;
+    t_b=0;
+    #5
+    //3
+    t_a=1;
+    t_b=1;
+    #5
+    $finish();
+  end
+endmodule
+    
