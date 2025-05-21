dcc15e0c92f78d2098bb4f88e9f4df63c2616b5d
jayasoundhar <148468173+jayasoundhar@users.noreply.github.com>
2025-05-21 15:02:30 +0530

    Delete DEMUX-1:4 directory

--- DEMUX-1:4/demux_1_4.v
+++
@@ -1,8 +0,0 @@
-// Code your design here
-module demux_1_4(output y0,y1,y2,y3, input s0,s1,x);
-  assign y0=(~s0&~s1)&x;
-  assign y1=(~s0&s1)&x;
-  assign y2=(s0&~s1)&x;
-  assign y3=(s0&s1)&x;
-endmodule
-  
