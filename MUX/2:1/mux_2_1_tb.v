fcb7bcaad06d07344c855d6419cb5f634c365125
jayasoundhar <148468173+jayasoundhar@users.noreply.github.com>
2025-05-21 12:27:05 +0530

    Update mux_2_1_tb.v

--- MUX/2:1/mux_2_1_tb.v
+++ MUX/2:1/mux_2_1_tb.v
@@ -1,24 +1,56 @@
 // Code your testbench here
 // or browse Examples
-module t_mux_1_4();
-  reg t_s0,t_s1,t_y0,t_y1,t_y2,t_y3;
-  wire t_x;
+module t_mux_2_1();
+  reg t_s,t_a,t_b;
+  wire t_y;
   
-  mux_4_1 dut(.s0(t_s0),.s1(t_s1),.x(t_x),.y0(t_y0),.y1(t_y1),.y2(t_y2),.y3(t_y3));
+  mux_2_1 dut(.a(t_a),.b(t_b),.s(t_s),.y(t_y));
   
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(1);
-    //$monitor("x=%b s0=%b s1=%b",t_x,t_s0,t_s1);
   end
   
   initial begin
-    t_s0=0;t_s1=0;t_y0=1;t_y1=0;t_y2=0;t_y3=0;#5
-    t_s0=0;t_s1=1;t_y0=0;t_y1=1;t_y2=0;t_y3=0;#5
-    t_s0=1;t_s1=0;t_y0=0;t_y1=0;t_y2=1;t_y3=0;#5
-    t_s0=1;t_s1=1;t_y0=0;t_y1=0;t_y2=0;t_y3=1;
-    #5$finish();
-     
+    t_a = 0;
+    t_b = 0;
+    t_s = 0;
+    #5
     
+    t_a = 0;
+    t_b = 0;
+    t_s = 1;
+    #5
+    
+    t_a = 1;
+    t_b = 0;
+    t_s = 0;
+    #5
+    
+    t_a = 0;
+    t_b = 0;
+    t_s = 1;
+    #5
+    
+    t_a = 0;
+    t_b = 1;
+    t_s = 0;
+    #5
+    
+    t_a = 0;
+    t_b = 1;
+    t_s = 1;
+    #5
+    
+    t_a = 1;
+    t_b = 1;
+    t_s = 1;
+    #5
+    
+    t_a = 1;
+    t_b = 1;
+    t_s = 0;
+    #5
+    $finish();
   end
 endmodule
