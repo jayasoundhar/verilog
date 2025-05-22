ae306433544a84206fbf9db2b6233003a1d73b82
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-22 16:51:22 +0530

    Update logic_gates.v

--- DAY-1/LOGIC-GATES/logic_gates.v
+++ DAY-1/LOGIC-GATES/logic_gates.v
@@ -1,1 +1,12 @@
-
+// Code your design here
+module logic_gates(output y_and,y_or,y_not,y_xor,y_xnor,y_nand,y_nor,input a,b);
+  assign y_and=a&b;
+  assign y_or=a|b;
+  assign y_not=~a;
+  assign y_nand=~(a&b);
+  assign y_nor=!(a|b);
+  assign y_xor=a^b;
+  assign y_nor=~(a^b);
+  
+endmodule
+  
