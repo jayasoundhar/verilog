7a545d09d5ae6c38315832eb7cd23b5609e19d14
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-26 12:27:39 +0530

    Create even_parity_checker_tb.v

---
+++ DAY-1/EVEN PARITY/even_parity_checker_tb.v
@@ -0,0 +1,28 @@
+// even parity checker.......
+
+module even_parity_generator_tb();
+  wire out;
+  reg [3:0] in;
+  integer i;
+  reg [0:15]check = 16'h6996;
+  
+  even_parity_generator dut(.in(in),.out(out));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("in=%b => out%b",in,out);
+  end
+  
+  initial begin
+    
+    for(i=0;i<16;i=i+1) begin
+      in=i;#5
+      if(out==check[i]) begin
+        $write("pass");
+      end else begin
+        $write("fail");
+      end
+    end
+    end
+endmodule
