af3e27daf4411decd9c8da4a3d533c58187609e4
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-27 15:35:52 +0530

    Create jk_latch_tb.v

---
+++ DAY 3/jk_latch_tb.v
@@ -0,0 +1,31 @@
+// jk latch....
+
+module jk_latch_tb();
+  reg j,k,clk;
+  wire q,qb;
+  initial begin 
+    clk = 0; 
+  end
+  
+  jk_latch dut(.j(j),.k(k),.q(q),.qb(qb),.clk(clk));
+  
+  always begin
+    #5clk = ~clk;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("clk=%b J=%b K=%b Q=%b Qb=%b",clk,j,k,q,qb);
+  end
+  
+  initial begin
+    
+    j=1;k=0;#10
+    j=0;k=0;#10
+    j=0;k=1;#10
+    j=0;k=0;#10
+    j=1;k=1;#10
+    $finish;
+  end
+endmodule
