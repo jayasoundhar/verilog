2cceb2812a344fc588265370ff0fd3d06c948ab5
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-27 15:44:53 +0530

    Create d_latch.v

---
+++ DAY 3/d_latch.v
@@ -0,0 +1,12 @@
+//D latch......
+module d_latch(output reg q,qb,input d,clk);
+  always@(posedge clk)begin
+ 
+    case(d)
+      1'b0 : q=0;
+      1'b1 : q=1;
+    endcase
+       qb = ~q;
+  end
+endmodule
+
