bd479019e926b56f69c218bf1f8bddc502a326d4
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-28 18:58:02 +0530

    Update siso.v

--- DAY 5/siso.v
+++ DAY 5/siso.v
@@ -1,1 +1,15 @@
-
+// SISO-right-shift....
+module siso(output reg out,input s,clk,rst);
+  reg [3:0]so;
+  always@(posedge clk) begin
+  if(rst) begin
+    out <= 1'b0;
+    so <= 4'b0000;
+  end
+  else begin
+    so <=so<<1;
+    so[0] <= s;
+    out <= so[3];    
+  end
+  end
+endmodule
