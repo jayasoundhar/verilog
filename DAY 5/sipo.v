3b94931f48e6d9ce78eb78c1c2d218b9df2f9b2a
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-28 19:10:21 +0530

    Create sipo.v

---
+++ DAY 5/sipo.v
@@ -0,0 +1,15 @@
+// SISO-right-shift....
+module siso(output reg [3:0]out,input s,clk,rst);
+  reg [3:0]so;
+  always@(posedge clk) begin
+  if(rst) begin
+    out <= 1'b0;
+    so <= 4'b0000;
+  end
+  else begin
+    so <=so<<1;
+    so[0] <= s;
+    out = so;  
+  end
+  end
+endmodule
