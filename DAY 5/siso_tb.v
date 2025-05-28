6d9da45643451203b9823df802df5f55191eca69
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-28 18:58:33 +0530

    Create siso_tb.v

---
+++ DAY 5/siso_tb.v
@@ -0,0 +1,32 @@
+// SISO.....
+module siso_tb();
+  reg clk,rst,s;
+  wire out;
+  initial clk=1;
+  
+  siso dut(.s(s),.clk(clk),.rst(rst),.out(out));
+  initial begin
+    repeat(15) begin
+      #5clk = ~clk;
+  end
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("rst=%b clk=%b s=%b => out=%b",rst,clk,s,out);
+  end
+  
+  initial begin
+    rst=0;s=1;#10
+    rst=0;s=1;#10
+    rst=0;s=1;#10
+    rst=0;s=0;#10
+    rst=0;s=1'bx;#10
+    rst=0;s=1'bx;#10
+    rst=0;s=1'bx;#10
+    rst=0;s=1'bx;#10
+    $finish;
+  end
+endmodule
+    
+    
