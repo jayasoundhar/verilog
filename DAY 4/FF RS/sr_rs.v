ba199e87fafd47cef2b8480a3676e880e01f66d6
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-28 10:38:37 +0530

    Update sr_rs.v

--- DAY 4/FF RS/sr_rs.v
+++ DAY 4/FF RS/sr_rs.v
@@ -1,1 +1,16 @@
-
+// SR ff...
+module sr_ff(output reg q,qb,input s,r,clk,rs);
+  
+  always@(posedge clk)begin
+ 	if(rs) begin
+  		q=0;
+    end
+    case({s,r})
+      2'b00 : q=q;
+      2'b01 : q=0;
+      2'b10 : q=1;
+      2'b11 : q=1'bx;
+    endcase
+       qb = ~q;
+  end
+endmodule
