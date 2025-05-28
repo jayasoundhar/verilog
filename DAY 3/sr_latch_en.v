23621ee5ab453f132dda731b4599a202f0411eb6
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-28 13:59:24 +0530

    Create sr_latch_en.v

---
+++ DAY 3/sr_latch_en.v
@@ -0,0 +1,14 @@
+// SR latch...
+module sr_latch(output reg q,qb,input s,r,en);
+  always@(*) begin
+    if(en) begin
+    case({s,r})
+      2'b00 : q=q;
+      2'b01 : q=0;
+      2'b10 : q=1;
+      2'b11 : q=1'bx;
+    endcase
+       qb = ~q;
+  end
+  end
+endmodule
