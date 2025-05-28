4f904cd3eec7ca0a9d6b4dbbcff84c56f0397fb7
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-28 14:09:27 +0530

    Create jk_en.v

---
+++ DAY 3/LATCH EN/jk_en.v
@@ -0,0 +1,13 @@
+//jk latch......
+module jk_latch(output reg q,qb,input j,k,en);
+  always@(*)begin
+ 
+    case({j,k})
+      2'b00 : q=q;
+      2'b01 : q=0;
+      2'b10 : q=1;
+      2'b11 : q=~q;
+    endcase
+       qb = ~q;
+  end
+endmodule
