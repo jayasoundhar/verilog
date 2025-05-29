244620b55cffc3dcfd08585d708e62bd31bb1b13
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-29 17:46:28 +0530

    Update upcounter.v

--- DAY 6/COUNTER/upcounter.v
+++ DAY 6/COUNTER/upcounter.v
@@ -1,1 +1,12 @@
+// up-Counter
+module up_counter(output reg [2:0]out,input rst,clk);
+  initial out = 3'bxxx;
+  always@(posedge clk) begin
+    if (rst) begin
+    out = 3'b000;
+  end
+  else
+    out = out+1;
+  end
+endmodule
 
