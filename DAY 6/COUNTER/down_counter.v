2005071a6d75877aa1f9266b62d308e1cebf678a
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-29 17:51:42 +0530

    Create down_counter.v

---
+++ DAY 6/COUNTER/down_counter.v
@@ -0,0 +1,11 @@
+// down-Counter
+module down_counter(output reg [2:0]out,input rst,clk);
+  initial out = 3'bxxx;
+  always@(posedge clk) begin
+    if (rst) begin
+    out = 3'b111;
+  end
+  else
+    out = out-1;
+  end
+endmodule
