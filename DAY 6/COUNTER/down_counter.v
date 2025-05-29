7ce5296e150a470293510a1e3af2d6f4d99e2756
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-29 17:59:56 +0530

    Update down_counter.v

--- DAY 6/COUNTER/down_counter.v
+++ DAY 6/COUNTER/down_counter.v
@@ -3,7 +3,7 @@ module down_counter(output reg [2:0]out,input rst,clk);
   initial out = 3'bxxx;
   always@(posedge clk) begin
     if (rst) begin
-    out = 3'b111;
+    out = 3'b000;
   end
   else
     out = out-1;
