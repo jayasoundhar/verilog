fcb308674a7439556dd295e4df697a230c7a48c3
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-05-29 09:54:53 +0530

    Update sipo.v

--- DAY 5/sipo.v
+++ DAY 5/sipo.v
@@ -1,15 +1,15 @@
-// SISO-right-shift....
-module siso(output reg [3:0]out,input s,clk,rst);
-  reg [3:0]so;
+// SIPO-right-shift....
+module sipo(output reg [3:0]out,input s,clk,rst);
+  reg [3:0]po;
   always@(posedge clk) begin
   if(rst) begin
     out <= 1'b0;
-    so <= 4'b0000;
+    po <= 4'b0000;
   end
   else begin
-    so <=so<<1;
-    so[0] <= s;
-    out = so;  
+    po <=po<<1;
+    po[0] <= s;
+    out = po;  
   end
   end
 endmodule
