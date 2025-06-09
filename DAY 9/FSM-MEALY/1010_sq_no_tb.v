fddee48cb2c2fef269caa95e48ee903575b7418b
JAYA SOUNDHAR V T <148468173+jayasoundhar@users.noreply.github.com>
2025-06-09 11:28:34 +0530

    Update 1010_sq_no_tb.v

--- DAY 9/FSM-MEALY/1010_sq_no_tb.v
+++ DAY 9/FSM-MEALY/1010_sq_no_tb.v
@@ -1,22 +1,23 @@
+
 module sq_menol_tb;
 reg in,clk,rst;
 wire out;
-sq_menol dut(in,clk,rst,out);
+  sq_menol dut(out,in,clk,rst);
 initial clk=0;
-always #2 clk=~clk;
+always #5 clk=~clk;
 initial begin
 $dumpfile("1010.vcd");
 $dumpvars;
-$monitor($time,"in=%d,clk=%d,rst=%d,out=%d",in,clk,rst,out);
+$monitor("in=%d,rst=%d,out=%d",in,rst,out);
 
 in=0;
-#10 rst =0; #10 rst=1;
+#10 rst =1; #10 rst=0;
 #10 in=1;
 #10 in=0;
 #10 in=1;
 #10 in=0;
-#9 in=1;
 #10 in=1;
-#75$finish;
+#10 in=1;
+$finish;
 end
 endmodule
